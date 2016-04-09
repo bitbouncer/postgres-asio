53bd1dca486276edb043db2f41208e5c12666c90
skarlsson <svante.karlsson@csi.se>
2016-04-09 21:01:10 +0200

    formatting

--- samples/cursor_sample/cursor_sample.cpp
+++ samples/cursor_sample/cursor_sample.cpp
@@ -8,81 +8,70 @@
 #include <boost/log/expressions.hpp>
 
 
-void handle_fetch1000(boost::shared_ptr<postgres_asio::connection> connection, size_t total_count, int ec, boost::shared_ptr<PGresult> res)
-{
-    if (ec)
-        return;
-    
-    int tuples_in_batch = PQntuples(res.get());
-    total_count += tuples_in_batch;
-    BOOST_LOG_TRIVIAL(info) << "got " << tuples_in_batch << ", total: " << total_count;
-    if (tuples_in_batch == 0)
-    {
-        BOOST_LOG_TRIVIAL(info) << "calling async_exec CLOSE mycursor; COMMIT....";
-        connection->exec("CLOSE mycursor; COMMIT", [connection](int ec, boost::shared_ptr<PGresult> res) 
-        { 
-            BOOST_LOG_TRIVIAL(info) << "async processing done";
-        });
-        return;
-    }
-    BOOST_LOG_TRIVIAL(info) << "calling async_exec FETCH 1000 in mycursor....";
-    connection->exec("FETCH 1000 in mycursor", [connection, total_count](int ec, boost::shared_ptr<PGresult> res) { handle_fetch1000(connection, total_count, ec, std::move(res)); });
+void handle_fetch1000(boost::shared_ptr<postgres_asio::connection> connection, size_t total_count, int ec, boost::shared_ptr<PGresult> res) {
+  if(ec)
+    return;
+
+  int tuples_in_batch = PQntuples(res.get());
+  total_count += tuples_in_batch;
+  BOOST_LOG_TRIVIAL(info) << "got " << tuples_in_batch << ", total: " << total_count;
+  if(tuples_in_batch == 0) {
+    BOOST_LOG_TRIVIAL(info) << "calling async_exec CLOSE mycursor; COMMIT....";
+    connection->exec("CLOSE mycursor; COMMIT", [connection](int ec, boost::shared_ptr<PGresult> res) {
+      BOOST_LOG_TRIVIAL(info) << "async processing done";
+    });
+    return;
+  }
+  BOOST_LOG_TRIVIAL(info) << "calling async_exec FETCH 1000 in mycursor....";
+  connection->exec("FETCH 1000 in mycursor", [connection, total_count](int ec, boost::shared_ptr<PGresult> res) { handle_fetch1000(connection, total_count, ec, std::move(res)); });
 }
 
-int main(int argc, char *argv[])
-{
-    std::string host;
-    boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::debug);
-    std::string connect_string = "user=postgres password=postgres dbname=test";
+int main(int argc, char *argv[]) {
+  std::string host;
+  boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::debug);
+  std::string connect_string = "user=postgres password=postgres dbname=test";
 
-    if (argc > 1)
-    {
-        connect_string += std::string("host=") + argv[1];
-    }
-   
-    boost::asio::io_service fg_ios;
-    boost::asio::io_service bg_ios;
-    std::auto_ptr<boost::asio::io_service::work> fg_work(new boost::asio::io_service::work(fg_ios)); // this keeps the fg_ios alive 
-    std::auto_ptr<boost::asio::io_service::work> bg_work(new boost::asio::io_service::work(bg_ios)); // this keeps the bg_ios alive
-    boost::thread fg(boost::bind(&boost::asio::io_service::run, &fg_ios));
-    boost::thread bg(boost::bind(&boost::asio::io_service::run, &bg_ios));
+  if(argc > 1) {
+    connect_string += std::string("host=") + argv[1];
+  }
 
-    auto connection = boost::make_shared<postgres_asio::connection>(fg_ios, bg_ios);
-    connection->set_warning_timout(100);
-    connection->connect(connect_string, [connection](int ec)
-    {
-        if (!ec)
-        {
-            BOOST_LOG_TRIVIAL(info) << "calling async_exec BEGIN";
-            connection->exec("BEGIN", [connection](int ec, boost::shared_ptr<PGresult> res)
-            {
-                if (ec)
-                {
-                    BOOST_LOG_TRIVIAL(error) << "BEGIN failed ec:" << ec << " last_error: " << connection->last_error();
-                    return;
-                }
-                BOOST_LOG_TRIVIAL(info) << "calling async_exec DECLARE mycursor....";
-                connection->exec("DECLARE mycursor CURSOR FOR SELECT * from postgres_asio_sample", [connection](int ec, boost::shared_ptr<PGresult> res)
-                {
-                    if (ec)
-                    {
-                        BOOST_LOG_TRIVIAL(error) << "DECLARE mycursor... failed ec:" << ec << " last_error: " << connection->last_error();
-                        return;
-                    }
-                    BOOST_LOG_TRIVIAL(info) << "calling async_exec FETCH 1000 in mycursor....";
-                    connection->exec("FETCH 1000 in mycursor", [connection](int ec, boost::shared_ptr<PGresult> res){ handle_fetch1000(connection, 0, ec, std::move(res)); });
-                });
-            });
+  boost::asio::io_service fg_ios;
+  boost::asio::io_service bg_ios;
+  std::auto_ptr<boost::asio::io_service::work> fg_work(new boost::asio::io_service::work(fg_ios)); // this keeps the fg_ios alive 
+  std::auto_ptr<boost::asio::io_service::work> bg_work(new boost::asio::io_service::work(bg_ios)); // this keeps the bg_ios alive
+  boost::thread fg(boost::bind(&boost::asio::io_service::run, &fg_ios));
+  boost::thread bg(boost::bind(&boost::asio::io_service::run, &bg_ios));
+
+  auto connection = boost::make_shared<postgres_asio::connection>(fg_ios, bg_ios);
+  connection->set_warning_timout(100);
+  connection->connect(connect_string, [connection](int ec) {
+    if(!ec) {
+      BOOST_LOG_TRIVIAL(info) << "calling async_exec BEGIN";
+      connection->exec("BEGIN", [connection](int ec, boost::shared_ptr<PGresult> res) {
+        if(ec) {
+          BOOST_LOG_TRIVIAL(error) << "BEGIN failed ec:" << ec << " last_error: " << connection->last_error();
+          return;
         }
-    });
+        BOOST_LOG_TRIVIAL(info) << "calling async_exec DECLARE mycursor....";
+        connection->exec("DECLARE mycursor CURSOR FOR SELECT * from postgres_asio_sample", [connection](int ec, boost::shared_ptr<PGresult> res) {
+          if(ec) {
+            BOOST_LOG_TRIVIAL(error) << "DECLARE mycursor... failed ec:" << ec << " last_error: " << connection->last_error();
+            return;
+          }
+          BOOST_LOG_TRIVIAL(info) << "calling async_exec FETCH 1000 in mycursor....";
+          connection->exec("FETCH 1000 in mycursor", [connection](int ec, boost::shared_ptr<PGresult> res) { handle_fetch1000(connection, 0, ec, std::move(res)); });
+        });
+      });
+    }
+  });
 
-    BOOST_LOG_TRIVIAL(debug) << "work reset";
-    bg_work.reset();
-    BOOST_LOG_TRIVIAL(debug) << "bg join";
-    bg.join();
+  BOOST_LOG_TRIVIAL(debug) << "work reset";
+  bg_work.reset();
+  BOOST_LOG_TRIVIAL(debug) << "bg join";
+  bg.join();
 
-    fg_work.reset();
-    BOOST_LOG_TRIVIAL(debug) << "fg join";
-    fg.join();
-    BOOST_LOG_TRIVIAL(debug) << "done";
+  fg_work.reset();
+  BOOST_LOG_TRIVIAL(debug) << "fg join";
+  fg.join();
+  BOOST_LOG_TRIVIAL(debug) << "done";
 }
--- samples/insert_sample/insert_sample.cpp
+++ samples/insert_sample/insert_sample.cpp
@@ -11,63 +11,55 @@
 #include <boost/uuid/uuid_generators.hpp>
 
 
-int main(int argc, char *argv[])
-{
-    boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::debug);
-    std::string connect_string = "user=postgres password=postgres dbname=test";
-    
-    if (argc > 1)
-    {
-        connect_string += std::string("host=") + argv[1];
-    }
+int main(int argc, char *argv[]) {
+  boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::debug);
+  std::string connect_string = "user=postgres password=postgres dbname=test";
 
-    boost::asio::io_service fg_ios;
-    boost::asio::io_service bg_ios;
-    std::auto_ptr<boost::asio::io_service::work> fg_work(new boost::asio::io_service::work(fg_ios)); // this keeps the fg_ios alive 
-    std::auto_ptr<boost::asio::io_service::work> bg_work(new boost::asio::io_service::work(bg_ios)); // this keeps the bg_ios alive
-    boost::thread fg(boost::bind(&boost::asio::io_service::run, &fg_ios));
-    boost::thread bg(boost::bind(&boost::asio::io_service::run, &bg_ios));
+  if(argc > 1) {
+    connect_string += std::string("host=") + argv[1];
+  }
 
-    // precondition CREATE TABLE postgres_asio_sample  ( id integer, val text )
+  boost::asio::io_service fg_ios;
+  boost::asio::io_service bg_ios;
+  std::auto_ptr<boost::asio::io_service::work> fg_work(new boost::asio::io_service::work(fg_ios)); // this keeps the fg_ios alive 
+  std::auto_ptr<boost::asio::io_service::work> bg_work(new boost::asio::io_service::work(bg_ios)); // this keeps the bg_ios alive
+  boost::thread fg(boost::bind(&boost::asio::io_service::run, &fg_ios));
+  boost::thread bg(boost::bind(&boost::asio::io_service::run, &bg_ios));
 
-    for (int i = 0; i != 50; ++i)
-    {
-        auto connection = boost::make_shared<postgres_asio::connection>(fg_ios, bg_ios);
-        std::string trace_id = "xxxx-xxxx-xx" + std::to_string(i);
-        connection->connect(connect_string, [connection](int ec)
-        {
-            if (!ec)
-            {
-                std::string statement = "insert into postgres_asio_sample (id, val) VALUES\n";
-                size_t items = 100;
-                for (int i = 0; i != items; ++i)
-                {
-                    const char* ch = i < (items - 1) ? ",\n" : ";\n";
-                    std::string val = to_string(boost::uuids::random_generator()());
-                    statement += " (" + std::to_string(i) + ", '" + val + "')" + ch;
-                }
+  // precondition CREATE TABLE postgres_asio_sample  ( id integer, val text )
 
-                BOOST_LOG_TRIVIAL(info) << "async_insert";
-                connection->exec(statement, [connection](int ec, boost::shared_ptr<PGresult> res)
-                {
-                    if (ec)
-                    {
-                        BOOST_LOG_TRIVIAL(error) << " insert failed ec:" << ec << " last_error:" << connection->last_error();
-                        return;
-                    }
-                    BOOST_LOG_TRIVIAL(info) << "async insert - done!!";
-                });
-            }
+  for(int i = 0; i != 50; ++i) {
+    auto connection = boost::make_shared<postgres_asio::connection>(fg_ios, bg_ios);
+    std::string trace_id = "xxxx-xxxx-xx" + std::to_string(i);
+    connection->connect(connect_string, [connection](int ec) {
+      if(!ec) {
+        std::string statement = "insert into postgres_asio_sample (id, val) VALUES\n";
+        size_t items = 100;
+        for(int i = 0; i != items; ++i) {
+          const char* ch = i < (items - 1) ? ",\n" : ";\n";
+          std::string val = to_string(boost::uuids::random_generator()());
+          statement += " (" + std::to_string(i) + ", '" + val + "')" + ch;
+        }
+
+        BOOST_LOG_TRIVIAL(info) << "async_insert";
+        connection->exec(statement, [connection](int ec, boost::shared_ptr<PGresult> res) {
+          if(ec) {
+            BOOST_LOG_TRIVIAL(error) << " insert failed ec:" << ec << " last_error:" << connection->last_error();
+            return;
+          }
+          BOOST_LOG_TRIVIAL(info) << "async insert - done!!";
         });
-    }
+      }
+    });
+  }
 
-    BOOST_LOG_TRIVIAL(debug) << "work reset";
-    bg_work.reset();
-    BOOST_LOG_TRIVIAL(debug) << "bg join";
-    bg.join();
+  BOOST_LOG_TRIVIAL(debug) << "work reset";
+  bg_work.reset();
+  BOOST_LOG_TRIVIAL(debug) << "bg join";
+  bg.join();
 
-    fg_work.reset();
-    BOOST_LOG_TRIVIAL(debug) << "fg join";
-    fg.join();
-    BOOST_LOG_TRIVIAL(debug) << "done";
+  fg_work.reset();
+  BOOST_LOG_TRIVIAL(debug) << "fg join";
+  fg.join();
+  BOOST_LOG_TRIVIAL(debug) << "done";
 }
